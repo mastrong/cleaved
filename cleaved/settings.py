0a580b2a607cbdccdec3c94f3de003c380cff8b2
mastrong <melissastrong89@gmail.com>
2024-04-18 15:30:26 -0400

    prod db name and rm sqlite3 from git

--- cleaved/settings.py
+++ cleaved/settings.py
@@ -81,11 +81,14 @@ WSGI_APPLICATION = 'cleaved.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
+if DEBUG:
+    DB_NAME = BASE_DIR / 'db.sqlite3'
+else:
+    DB_NAME = '/home/db.sqlite3'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': DB_NAME,
     }
 }
 
